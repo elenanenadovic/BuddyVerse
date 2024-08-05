d42fd076405dc9324b4eb6538f4ec8c27c49e365
elenanenadovic <elenanenadovic@gmail.com>
2024-08-05 14:48:40 +0200

    finished fetching games

--- frontend/src/CustomComponents/GamesView.js
+++ frontend/src/CustomComponents/GamesView.js
@@ -2,11 +2,11 @@ import { Component } from "react";
 import axios from "axios";
 
 class GameView extends Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props)
-    this.state={
-      games:[]
+    this.state = {
+      games: []
     }
   }
 
@@ -16,40 +16,38 @@ class GameView extends Component {
     this.props.QIDFromChild(obj);
   };
 
-  
+
   /*rusn everytime component is mounted*/
-  
-  componentDidMount(){
+
+  componentDidMount() {
     axios.get("http://88.200.63.148:4567/games")
-    .then(res=>{
-      this.setState({
-        games: res.data
+      .then(res => {
+        this.setState({
+          games: res.data
+        })
       })
-    })
   }
 
   render() {
-    
+    console.log(this.state.games)
+    let data = this.state.games
     return (
-      <div
-        className="row row-cols-1 row-cols-md-3 g-4"
-        style={{ margin: "10px" }}
-      >
-        <div className="col">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Slug</p>
+      <div className="row row-cols-1 row-cols-md-3 g-4" style={{ margin: "10px" }}>
+        {data.length > 0 ?
+          data.map(d => {
+            return (<div className="col">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{d.name}</h5>
+                  <p className="card-text">{d.description}</p>
+                </div>
+                <button
+                onClick = {() => this.QSetViewInParent({page: "game", id: d.id})} style={{ margin: "10px" }} className="btn btn-primary bt">Read more</button>
+              </div>
             </div>
-            <button
-              onClick={() => this.QSetViewInParent({ page: "game", id: 1 })}
-              style={{ margin: "10px" }}
-              className="btn btn-primary bt"
-            >
-              Read more
-            </button>
-          </div>
-        </div>
+            )
+          }) :
+          "loading"}
       </div>
     );
   }
